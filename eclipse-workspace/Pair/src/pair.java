11350229dba9125602c8f9057fb6750b2ebceb4f
athira c <athira.c12345@gmail.com>
2021-11-05 21:40:13 +0530

    commit

---
+++ eclipse-workspace/Pair/src/pair.java
@@ -0,0 +1,51 @@
+
+
+	class Main{
+	    public static void main(String[] args) {
+	        pair myObj = new pair();
+	        myObj.setKey("13829");
+	        myObj.setValue("only String value");
+
+	        // set the date
+
+	        myObj.setKey("Today is ");
+	        myObj.setValue(new java.util.Date().toString());     // convert to string
+
+	        System.out.println(myObj);
+	    }
+	}
+
+
+
+
+	public class pair
+	{
+	    private String key;
+	    private String value;
+
+	    public String getKey() {
+	        return key;
+	    }
+
+	    public void setKey(String key) {
+	        this.key = key;
+	    }
+
+	    public String getValue() {
+	        return value;
+	    }
+
+	    public void setValue(String value) {
+	        this.value = value;
+	    }
+
+	    @Override
+	    public String toString() {
+	        return "Pair { " + key + " "+ value + " }";
+	    }
+	}
+	
+
+
+
+
